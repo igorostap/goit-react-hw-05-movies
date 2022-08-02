880ada059928b7bb69386b699d07eeb6ba9487e1
igorostap <92843339+igorostap@users.noreply.github.com>
2022-08-02 20:14:55 +0000

    Deploying to gh-pages from @ igorostap/goit-react-hw-05-movies@4ca5b3e004e1bd0dcb11bb67807eeb3e91565d5c 🚀

---
+++ static/js/949.70e492c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[949],{4949:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(3504),o=n(184);function u(){return(0,o.jsx)(r.rU,{to:"/",children:"Go Home"})}}}]);
+//# sourceMappingURL=949.70e492c3.chunk.js.map
